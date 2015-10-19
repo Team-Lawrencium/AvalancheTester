69639285f380e49401eba7daa5c108d6f0cc4756
damy90 <dany90@gmail.com>
2015-10-20 01:37:12 +0300

    added xml tests report variants

--- AvalancheTester/AvalancheTester.Application/XmlReports.cs
+++ AvalancheTester/AvalancheTester.Application/XmlReports.cs
@@ -10,16 +10,48 @@ namespace AvalancheTester.Application
 {
     public class XmlReports
     {
-        private const string path = "../../../XML reports/xml-report";
+        private const string path = "../../../XML reports/xml-report.xml";
 
-        public void LastYear(AvalancheTestsDbEntities db, Place place, DateTime today)
+        public void LastMonth(Place place, DateTime today)
         {
+            AvalancheTestsDbEntities db = new AvalancheTestsDbEntities();
+
             List<Test> tests = db.Tests
                 .Where(t => t.Place.Name == place.Name)
-                .Where(t => (t.Date >= today.AddYears(-1) && t.Date <= today))
+                .Where(t => (t.Date >= today.AddMonths(-1) && t.Date <= today))
                 .ToList();
 
-            var xmlSerializer=new XmlSerializer(tests.GetType());
+            this.XmlSerializeResult(tests, "monthly-tests-"+place.Name+".xml");
+        }
+
+        public void LastWeek(Place place, DateTime today)
+        {
+            AvalancheTestsDbEntities db = new AvalancheTestsDbEntities();
+
+            List<Test> tests = db.Tests
+                .Where(t => t.Place.Name == place.Name)
+                .Where(t => (t.Date >= today.AddDays(-7) && t.Date <= today))
+                .ToList();
+
+            this.XmlSerializeResult(tests, "weekly-tests-"+place.Name+".xml");
+        }
+
+        public void LastYear(Place place, DateTime today)
+        {
+            AvalancheTestsDbEntities db = new AvalancheTestsDbEntities();
+
+            var tests = db.Tests
+                .Where(t => t.Place.Name == place.Name)
+                .Where(t => (t.Date >= today.AddYears(-1) && t.Date <= today))
+                .GroupBy(gr => new {gr.Date.Year, gr.Date.Month })
+                .ToList();
+
+            this.XmlSerializeResult(tests, "yearly-tests-"+place.Name+".xml");
+        }
+
+        private void XmlSerializeResult(dynamic tests, string fileName)
+        {
+             var xmlSerializer=new XmlSerializer(tests.GetType());
             StreamWriter file = new StreamWriter(path);
 
             xmlSerializer.Serialize(file, tests);
